7037ff2cf2366e2fda7cee2d5b86913d41421814
Frederik Hahne <frederik.hahne@gmail.com>
2016-04-18 19:34:28 +0200

    inital commit

---
+++ src/main/java/com/github/atomfrede/vertx/publisher/Publisher.java
@@ -0,0 +1,41 @@
+package com.github.atomfrede.vertx.publisher;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.NetworkConfig;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+
+public class Publisher extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+        HazelcastClusterManager mgr = new HazelcastClusterManager();
+
+        Config conf = new Config();
+
+        NetworkConfig networkConfig = new NetworkConfig();
+        networkConfig.setPort(5702);
+        networkConfig.setPortAutoIncrement(false);
+
+        conf.setNetworkConfig(networkConfig);
+        mgr.setConfig(conf);
+
+        mgr.setConfig(conf);
+
+        VertxOptions options = new VertxOptions().setClusterManager(mgr);
+
+        Vertx.clusteredVertx(options, res -> {
+            if (res.succeeded()) {
+                Vertx vertx = res.result();
+
+                vertx.setPeriodic(10000, t -> vertx.eventBus().publish("test.ping", "ping from publisher"));
+
+            } else {
+                // failed!
+            }
+        });
+    }
+}
